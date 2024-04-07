53fdf5d4316a28934ba0c644f6ea7418904145fd
Gabriel Bustillo del Cuvillo <gabibust@gmail.com>
2024-04-07 12:41:19 +0200

    Implemented Get a CSRF from Backloggd Site (Temp XPath), Proxy Debugging and cookieContainer and fill some fields.

--- BackloggdIntegration/MainBacklog.cs
+++ BackloggdIntegration/MainBacklog.cs
@@ -5,39 +5,75 @@ using System.Net.Http.Headers;
 using GameListDB.Model;
 using GameListDB.Model.Extensions;
 using GameListDB.DTO;
+using HtmlAgilityPack;
 
 namespace GameListDB.BackloggdIntegration
 {
 
   public class BackloggdExporter
   {
+    private const string _DebugUsername = "";
     private readonly HttpClient _client;
+    private CookieContainer cookieContainer;
     private readonly Backloggd backloggdConfig;
+    string csrf = string.Empty;
+
     private readonly GameListsContext gamelistdb;
 
-    public BackloggdExporter(GameListsContext gameListsContext, Config config) 
+    public BackloggdExporter(GameListsContext gameListsContext, Config config)
     {
       gamelistdb = gameListsContext;
       backloggdConfig = config.BACKLOGGD;
+      cookieContainer = new CookieContainer();
 
-      _client = new();
+      String proxyURL = "http://localhost:8080";
+      HttpClientHandler httpClientHandler = new HttpClientHandler
+      {
+        CookieContainer = cookieContainer,
+        UseCookies = true,
+
+        // Only for Debugging
+        #if DEBUG
+        Proxy = new WebProxy(proxyURL),
+        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        #endif
+      };
+
+      _client = new(httpClientHandler);
       _client.DefaultRequestHeaders.Accept.Clear();
-      
+
       _client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36");
       _client.DefaultRequestHeaders.Add("Origin", "https://www.backloggd.com/");
       _client.DefaultRequestHeaders.Add("Referer", "https://www.backloggd.com/");
-      _client.DefaultRequestHeaders.Add("Cookie", 
-                  "cookies_consent=true;remember_user_token=1;"+
-                  "_august_app_session='"+backloggdConfig.COOKIE_TOKEN+"';"+
-                  "_backloggd_session='"+backloggdConfig.BACKLOGGD_SESSION+"';"+
-                  "remember_user_token='"+backloggdConfig.REMEMBER_USER_TOKEN+"'"
+      _client.DefaultRequestHeaders.Add("Cookie",
+                  //"cookies_consent=true;remember_user_token=1;" +
+                  "ne_cookies_consent=false;" +
+                  "advanced_open=false;" +
+                  "daily_tip=%2C%20your%20collection%20awaits...;" +
+                  "_august_app_session=" + backloggdConfig.COOKIE_TOKEN + ";" +
+                  "_backloggd_session=" + backloggdConfig.BACKLOGGD_SESSION + ";" +
+                  "remember_user_token=" + backloggdConfig.REMEMBER_USER_TOKEN + ";" +
+                  "game-log-editor-mode='quick';"
                   );
-        _client.DefaultRequestHeaders.Add("X-CSRF-Token", backloggdConfig.CSRF_TOKEN);
+    }
+
+    private async Task CRFSetter(string htmlDocument)
+    {
+      
+      HtmlDocument htmlSnippet = new HtmlDocument();
+      htmlSnippet.LoadHtml(htmlDocument);
+      csrf = htmlSnippet.DocumentNode.SelectSingleNode("/html/head/meta[15]").Attributes["content"].Value;
 
     }
 
     public async Task RunAsync()
     {
+      var initRequest = await _client.GetAsync("https://www.backloggd.com/")
+                        .ConfigureAwait(false).GetAwaiter().GetResult()
+                        .Content.ReadAsStringAsync();
+      if (! initRequest.Contains(_DebugUsername)) {Console.WriteLine("User Cookie Doesn't work") ; throw new Exception("User Cookie doesn't work"); }
+      await CRFSetter(initRequest);
+
       foreach (var game in gamelistdb.GetBeatenGames(2024))
       {
         var nvc = new List<KeyValuePair<string, string>>()
@@ -58,7 +94,7 @@ namespace GameListDB.BackloggdIntegration
           new KeyValuePair<string, string>("playthroughs[0][finish_date]", ""),
           // END STUB
 
-          new KeyValuePair<string, string>("log[is_play]", (game.Status == "Beaten").ToString().ToLower()),
+          new KeyValuePair<string, string>("log[is_play]", (game.Status == "Beaten" || game.Status == "Finished").ToString().ToLower()),
           new KeyValuePair<string, string>("log[is_playing]", (game.Status == "InProgress").ToString().ToLower()),
           new KeyValuePair<string, string>("log[is_backlog]", (game.Status == "NotStarted").ToString().ToLower()),
           new KeyValuePair<string, string>("log[is_wishlist]", "false"),
@@ -67,10 +103,15 @@ namespace GameListDB.BackloggdIntegration
           new KeyValuePair<string, string>("modal_type", "quick"),
 
         };
-      
-      var debug = await _client.PostAsync(
-                GetURL(gamelistdb.GetIgdbId(game).ToString()),
-                new FormUrlEncodedContent(nvc)).ConfigureAwait(false);
+        var message = new HttpRequestMessage(HttpMethod.Post, 
+        GetURL(gamelistdb.GetIgdbId(game).ToString())) { Content = new FormUrlEncodedContent(nvc) };
+        message.Headers.Add("X-CSRF-Token", csrf);
+      message.Headers.Add("X-Requested-With", "XMLHttpRequest");
+      message.Headers.Add("Sec-Fetch-Dest", "empty");
+      message.Headers.Add("Sec-Fetch-Mode", "cors");
+      message.Headers.Add("Sec-Fetch-Site", "same-origin");
+
+      var debug = await _client.SendAsync(message).ConfigureAwait(false).GetAwaiter().GetResult().Content.ReadAsStringAsync();
       }
     }
 
